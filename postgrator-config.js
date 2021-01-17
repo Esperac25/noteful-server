cc6b2a970f9cbfec3eda15af71c8ebb8098152fb
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-01-17 11:33:25 -0800

    updates

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DATABASE_URL
+  "connectionString": 'postgres://hydervkufzddbn:508bb3df8695a81ebd11b3918c3868e9747e05eeee0cad80885376adc2153da4@ec2-52-22-135-159.compute-1.amazonaws.com:5432/d54hbetl514b61'
 };
